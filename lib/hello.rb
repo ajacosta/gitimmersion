f3efbc8da003f57a4cbe605d46b1af41416c62bf
ajacosta <acosta.pmp@gmail.com>
2014-04-05 17:12:03 -0600

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,6 @@
+require 'greeter'
 # Default is "World"
 # Author: ajacosta (acosta.pmp@gmail.com)
 name = ARGV.first || "World"
-puts "Hello, #{name} !"
+greeter = Greeter.new(name)
+puts greeter.greet 
